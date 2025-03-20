758b5b146258f8b215e54d265ac334d3a503f972
panchalom78 <panchalom787@gmail.com>
2025-03-20 14:21:24 +0530

    update AssemblyInfo and sourcelink for version 1.0.0+028ad73

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0546e672628ef518134079621e3eb9b82bd5f4f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+028ad73a18cd584e25bbcab34f041ef297f14be7")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
